e8f4df72e0e225c2fcc11294a72180b3a167231e
George Nyakundi <dev.things.ke@gmail.com>
2020-12-17 11:33:13 +0300

    chore(init): adding app shell

---
+++ PayPalApp/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayPalApp
+//
+//  Created by George  on 17/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
